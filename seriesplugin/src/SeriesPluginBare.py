5802989d52b5b0e9e9f50ee3a8dd3258eca1018c
Hains <hainsvdbosch@ziggo.nl>
2020-04-20 19:23:55 +0200

    Fix global is used prior to global declaration
    global baseInfoBarShowHide__init__, RecordService, _session
    |     ^
    | SyntaxError: name '_session' is used prior to global declaration

--- seriesplugin/src/SeriesPluginBare.py
+++ seriesplugin/src/SeriesPluginBare.py
@@ -44,13 +44,11 @@ def bareGetEpisode(service_ref, name, begin, end, description, path, future=True
 			return (name, description, path, log.get())
 		
 		elif data and isinstance(data, basestring):
-			global loop_data
 			msg = _("Failed: %s." % ( str( data ) ))
 			log.debug(msg)
 			loop_data.append( name + ": " + msg )
 		
 		else:
-			global loop_data
 			msg = _("No data available")
 			log.debug(msg)
 			loop_data.append( name + ": " + msg )
