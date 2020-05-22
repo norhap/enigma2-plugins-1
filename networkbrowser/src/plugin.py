e1f1471dc280b6f35efbf98d753f476cee0e58e8
norhap <alvaro@elfogondesegovia.com>
2020-05-22 06:37:44 +0200

    test items menu

--- networkbrowser/src/plugin.py
+++ networkbrowser/src/plugin.py
@@ -23,7 +23,7 @@ class MountAgainCheckPoller:
 
 	def onTimer(self):
 		self.timer.stop()
-		if config.networkbrowser.automountpoll.value:
+		if hasattr(self, "networkbrowser") and config.networkbrowser.automountpoll.value:
 			self.mountagaincheck()
 		else:
 			self.timer.startLongTimer(30*60)
@@ -55,7 +55,7 @@ class MountAgainCheckPoller:
 
 	def JobEpgCache(self):
 		print '[Networkbrowser MountAgain] mounting network shares.'
-		iAutoMount.getAutoMountPoints() 
+		iAutoMount.getAutoMountPoints()
 
 	def JobSched(self):
 		self.timer.startLongTimer(int(config.networkbrowser.automountpolltimer.value) * 3600)
@@ -84,7 +84,7 @@ def MountManagerCallFunction(iface):
 
 def RemountMain(session, iface = None, **kwargs):
 	from AutoMount import iAutoMount
-	iAutoMount.getAutoMountPoints() 
+	iAutoMount.getAutoMountPoints()
 
 def RemountCallFunction(iface):
 	return RemountMain
